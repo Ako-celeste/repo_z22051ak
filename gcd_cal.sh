f835642d3bfa821d14ea1ea3d594e77dea5d5d8a
Ako KAJIWARA <z22051ak@aiit.ac.jp>
2022-07-31 10:50:30 +0900

    更新しました0731

--- gcd_cal.sh
+++ gcd_cal.sh
@@ -4,25 +4,23 @@
 #２つの数字を入力
 echo gcdを算出します。２つの数字を入力し、間にスペースを入れてください
 
-#読み込む
 read a b
 
         m=$a
 
 #bがmより小さい値かチェックする
 #イエスの場合mに新しい値を代入する
-if [ " $b" -lt "$m" ]
+if [ $b -lt $m ]
 then
 	m=$b
 fi
 
-#ループ処理で最大公約数gcdをチェックする
-while [ "$m" -ne " 0" ]
+while [[ $m -ne 0 ]]
 do
 	x=`expr $a % $m`
 	y=`expr $b % $m`
 
-	#もしxとyが両方とも0なら処理を終える
+	
 	#実行し最大公約数gcdを算出する
 	if [ $x -eq 0 -a $y -eq 0 ]
 	then
--- gcd_cal_test.sh
+++ gcd_cal_test.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 
 
+./gcd_cal.sh a b > /tmp/$$-result
+
+
+#引数の数をチェック（２個かどうか）
+if [ $#!=1 ]; then
+    echo "args count error!" > /tmp/$$-ans
+　　exit 1
+else
+    echo "OK"
+fi
+
+  
+#入力した引数が同じ数値でないかどうか
+if [ $1 -eq $2 ]; then
+     echo "数字が重複しています。違う数値を入れて下さい" > /tmp/$$-ans
+　 exit 1
+fi
+
