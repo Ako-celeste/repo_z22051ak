f1cd6b48c6921b928991514afefbd63ff3a143b8
Ako KAJIWARA <z22051ak@aiit.ac.jp>
2022-08-01 23:04:46 +0900

    改良ver5

--- gcd_cal_test.sh
+++ gcd_cal_test.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-./gcd_cal.sh > /tmp/$$-result
-
-tmp=/tmp/$$
-
-echo "input 2 argments" > $tmp-args
-echo "two numbers are the same" > $tmp-same
-
 
 ERROR_EXIT() {
    echo "$1" >&2
@@ -15,16 +8,16 @@ ERROR_EXIT() {
 }
 
 
-#テスト１引数の数をチェック（２個かどうか）
-./gcd_cal.sh 2 > $tmp-ans && ERROR_EXIT "error in test1-1"
-diff $tmp-ans $tmp-args || ERROR_EXIT "error in test1-2"
- 
+tmp=/tmp/$$
 
 
-#テスト２入力した引数が同じ数値でないかどうか
-./gcd_cal.sh 2 2 > $tmp-ans && ERROR_EXIT "error in test2-1"
-diff $tmp-ans $tmp-same || ERROR_EXIT "error in test2-2"
+./gcd_cal.sh > $tmp-result
+echo "input 2 argments" > $tmp-args
+echo "two numbers are the same" > $tmp-same
+diff $tmp-args $tmp-result || ERROR_EXIT "error in test1"
+diff $tmp-same $tmp-result || ERROR_EXIT "error in test2"
 
 
 
+rm -f /$tmp-*
    
