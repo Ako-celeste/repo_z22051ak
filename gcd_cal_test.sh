0c624f09736ea7987db265a4dfd1d357a03e9d20
Ako KAJIWARA <z22051ak@aiit.ac.jp>
2022-08-02 20:49:34 +0900

    revised0802

--- gcd_cal_test.sh
+++ gcd_cal_test.sh
@@ -5,16 +5,13 @@ tmp=/tmp/$$
 
 
 #test1
-echo "input 2 argments" > $tmp-args
-./gcd_cal.sh 24 > $tmp-result
-diff /$tmp-args $tmp-result && echo "テスト1正常入力" || exit 1
+ echo "input 2 argments" > $tmp-ans
+ ./gcd_cal.sh a > $tmp-result
+ diff /$tmp-ans $tmp-result || exit 1
 
-#test2
-echo "two numbers are the same" > $tmp-same
-./gcd_cal.sh 24 24 > $tmp-result
-diff $tmp-same $tmp-result && echo "テスト2正常入力" || exit 1
-
-
-rm -f /$tmp-*
+ #test2
+ echo "two numbers are the same" >> $tmp-ans
+ ./gcd_cal.sh a a >> $tmp-result
+ diff $tmp-ans $tmp-result || exit 1  
 
    
