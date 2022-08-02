b528ed19452561465f247907dafd34d24ce11175
Ako KAJIWARA <z22051ak@aiit.ac.jp>
2022-08-02 09:31:19 +0900

    ver0802

--- gcd_cal_test.sh
+++ gcd_cal_test.sh
@@ -4,20 +4,17 @@
 tmp=/tmp/$$
 
 
-ERROR_EXIT() {
-   echo "$1" >&2
-   rm -f $tmp-*
-   exit 1
-}
-
-
-./gcd_cal.sh > $tmp-result
+#test1
 echo "input 2 argments" > $tmp-args
+./gcd_cal.sh 24 > $tmp-result
+diff /$tmp-args $tmp-result && echo "テスト1正常入力" || exit 1
+
+#test2
 echo "two numbers are the same" > $tmp-same
-diff $tmp-args $tmp-result || ERROR_EXIT "error in test1"
-diff $tmp-same $tmp-result || ERROR_EXIT "error in test2"
+./gcd_cal.sh 24 24 > $tmp-result
+diff $tmp-same $tmp-result && echo "テスト2正常入力" || exit 1
 
 
+rm -f /$tmp-*
 
-rm -f $tmp-*
    
