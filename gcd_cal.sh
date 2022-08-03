695c422fc08e782b4d3041f9dbd95fa0675e78c6
Ako KAJIWARA <z22051ak@aiit.ac.jp>
2022-08-03 14:21:21 +0900

    改良0803

--- gcd_cal.sh
+++ gcd_cal.sh
@@ -26,18 +26,18 @@ fi
 
 while [ "$m" -ne 0 ]
 do
-	x=`expr $1 % $m`
-	y=`expr $2 % $m`
+	x=$(expr "$1" % "$m")
+	y=$(expr "$2" % "$m")
 
 	
 	
-if [ $x -eq 0 -a $y -eq 0 ]
+if [ "$x" -eq 0 -a "$y" -eq 0 ]
 then
 	
-	echo gcd of $1 and $2 is $m
+	echo gcd of "$1" and "$2" is "$m"
 		break
 	fi
-	m=`expr $m - 1`
+	m=$(expr "$m" - 1)
 
 done
 
