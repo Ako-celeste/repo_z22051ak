d6bda8300b8a5cbbeb86cd5fab9bc3e8e30a944c
Ako KAJIWARA <z22051ak@aiit.ac.jp>
2022-08-03 22:09:04 +0900

    0803

--- gcd_cal_test.sh
+++ gcd_cal_test.sh
@@ -9,9 +9,4 @@ tmp=/tmp/$$
  ./gcd_cal.sh a > $tmp-result
  diff $tmp-ans $tmp-result || exit 1
 
- #test2
- echo "two numbers are the same" > $tmp-same
- ./gcd_cal.sh a a > $tmp-result2
- diff $tmp-same $tmp-result2 || exit 1  
-
  
